247afc417cc9da6bc4024b210b3b77be0a0b85c1
Koomis <tonykelanen@gmail.com>
2025-10-22 21:00:34 +0300

    Fix psycopg and Render DB config

--- suppliers/settings.py
+++ suppliers/settings.py
@@ -90,8 +90,9 @@ DATABASES = {
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/suppliersdb',
+        default=os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/suppliersdb'),
         conn_max_age=600
+        ssl_require=True
     )
 }
 
