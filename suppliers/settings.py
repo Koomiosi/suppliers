303e7993fe0b6784472107091b6df2fd3a647a3f
Koomis <tonykelanen@gmail.com>
2025-10-22 21:03:00 +0300

    Fix psycopg and Render DB config

--- suppliers/settings.py
+++ suppliers/settings.py
@@ -89,10 +89,9 @@ DATABASES = {
 # Render käytössä
 DATABASES = {
     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
         default=os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/suppliersdb'),
-        conn_max_age=600
-        ssl_require=True
+        conn_max_age=600,
+        ssl_require=True  # tärkeä Renderissä, koska PostgreSQL vaatii SSL:n
     )
 }
 
