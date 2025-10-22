57381487898a7b2a8acfbdef919d5d8821a7c9e6
Koomis <tonykelanen@gmail.com>
2025-10-22 20:37:28 +0300

    settings.py modified

--- suppliers/settings.py
+++ suppliers/settings.py
@@ -76,19 +76,35 @@ WSGI_APPLICATION = 'suppliers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# Replace the SQLite DATABASES configuration with PostgreSQL:
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/suppliers',
-        conn_max_age=600
-    )
-}
 '''
+# Perus SQLite
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+# Render käytössä
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/suppliersdb',
+        conn_max_age=600
+    )
+}
+
+'''
+# Lokaali PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_suppliers_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Simpxxyyyy!', #vaihda oikea
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 '''
